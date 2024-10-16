05a4f651e45a5d36d8a94e5b370d7732598f6cdf
Omar <omar.ahmed2004@outlook.com>
2024-10-17 01:11:33 +0300

    Updated final view for product and updated Layout

--- Inventory/obj/Debug/net8.0/Inventory.AssemblyInfo.cs
+++ Inventory/obj/Debug/net8.0/Inventory.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed3e43624d6518c22e0eb63f91b53cc7052f1b99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+964f939f2237e7733e2301af6fec3690c8076f34")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
