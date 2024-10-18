85f2b8c7eb4c028e476d6cc18d769c7bcd3a880b
Omar <omar.ahmed2004@outlook.com>
2024-10-18 19:19:21 +0300

    Create new product appears only for admin

--- Inventory/obj/Debug/net8.0/Inventory.AssemblyInfo.cs
+++ Inventory/obj/Debug/net8.0/Inventory.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c08a2717901183ccac22d8d8b000ef156b1474e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11088e35cd5f9cbb8a330f9805155c631259a1a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
